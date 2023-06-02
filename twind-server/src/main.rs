eef3111bb526ecf4c96902a3ca3dd0dd290130b4
Arata <arata04962@gmail.com>
2023-06-02 17:20:44 +0900

    feat: add repl

--- twind-server/src/main.rs
+++ twind-server/src/main.rs
@@ -1,17 +1,33 @@
-use std::{env, fs, process::exit};
-
-use interpreter::interpret;
+use std::{
+    env, fs,
+    io::{stdin, stdout, Write},
+};
 
 mod interpreter;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    if args.len() < 2 {
-        println!("Usage: {} [filename]", args[0]);
-        exit(1);
-    }
 
-    let s = fs::read_to_string(&args[1]).expect("cannot read file");
-    let value = interpret(&s);
-    println!("{value:?}")
+    if args.len() < 2 {
+        repl_loop();
+    } else {
+        let s = fs::read_to_string(&args[1]).expect("cannot read file");
+        let value = interpreter::interpret(&s);
+        println!("{value:?}")
+    }
+}
+
+fn repl_loop() {
+    loop {
+        print!("> ");
+        stdout().flush().expect("failed to stdout::flush");
+
+        let mut s = String::new();
+        stdin()
+            .read_line(&mut s)
+            .expect("failed to stdin::read_line");
+
+        let value = interpreter::interpret(&s);
+        println!("{value:?}");
+    }
 }
